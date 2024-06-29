52ddd3c2f95ec18d541be4fecee82776f15618e5
Michal Lauer <michal.lauer.25@gmail.com>
2024-06-29 10:01:46 +0200

    Handles NaN and undefined results

--- R/fnc_char.R
+++ R/fnc_char.R
@@ -1,7 +1,15 @@
 char <- function(x, replace = " - ") {
   if (is.function(x)) {
-    tryCatch(x(), error = function(e) replace)
-  } else if (length(x) == 0) {
+    tryCatch({
+      res <- x()
+      if (is.nan(res)) {
+        return(replace)
+      } else {
+        return(res)
+      }
+     
+    }, error = function(e) replace)
+  } else if (length(x) == 0 || x == 'NaN' || x == 'undefined') {
     replace
   } else {
     x
